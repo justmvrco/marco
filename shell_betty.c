06460b97883309a33e57b971dd2d9cfbe4cdba0c
justmvrco <moreirapiliki@gmail.com>
2021-08-21 12:53:14 -0700

    marco

---
+++ shell_betty.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point for the shell
+ * @argc: arguments
+ * @argv: array of the arguments
+ * Return: 1 or 0
+ */
+int main(int argc, char *argv[])
+{
+
+	return (0);
+}
